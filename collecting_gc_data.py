d49a15a6b7f1d4bf6798f9b1b2ccc310b73df4c8
shaharmeromconsulting <101998799+shaharmeromconsulting@users.noreply.github.com>
2022-11-07 23:10:20 +1100

    Create collecting_gc_data.py
    To make this script work need to install selenium, pandas and webdriver_manager. I do everything through anaconda, but this is optional.
    
    Uses selenium to log in to nfl fantasy and collect all results for every game since 2012, until 2022.
    Still need to:
    - Add real credentials in lines 11 and 12
    - Add real path in line 54
    - Create modified script to only scrape new information instead of repeating entire history, and append data to table

---
+++ collecting_gc_data.py
@@ -0,0 +1,56 @@
+#Load packages
+from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+from webdriver_manager .chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+import pandas as pd
+driver = webdriver.Chrome(executable_path=ChromeDriverManager().install())
+
+#credentials
+username = 'USER_EMAIL'
+password = 'PASSWORD'
+
+#Pass credentials
+driver.get('https://id.nfl.com/account/sign-in')
+inputs = driver.find_elements(By.TAG_NAME,'input')
+inputs[0].send_keys(username)
+inputs[1].send_keys(password)
+sign_in = driver.find_elements(By.CLASS_NAME,"css-1dbjc4n")
+sign_in = [s for s in sign_in if s.text == 'Sign In'][0]
+a = ActionChains(driver).move_to_element(sign_in).double_click()
+a.perform()
+
+# Get aribrary round (For some reason the first time you get this link after logging in, NFL
+# redirects you
+driver.get('https://fantasy.nfl.com/league/1078038/history/2022/teamgamecenter?teamId=1&week=8')
+
+#Collect data as dictionary
+main_dict = {}
+for year in range(2012,2023):
+    round_dict={}
+    for round in range(1,17):
+        driver.get('https://fantasy.nfl.com/league/1078038/history/{year}/teamgamecenter?teamId=1&week={round}'.format(year=year,round=round))
+   
+        games_dict ={}
+        games = driver.find_elements(By.CLASS_NAME,'dynamic')
+        games_details = [s.find_elements(By.TAG_NAME,'div') for s in games][0:8]
+        i=0
+        for g in games_details:
+            try:
+                detail_dict = {'year':year,
+                               'round':round,
+                               'game':i+1,
+                               'home_team':g[0].find_element(By.TAG_NAME,'em').text,
+                               'away_team':g[1].find_element(By.TAG_NAME,'em').text,
+                               'home_score':g[0].find_element(By.TAG_NAME,'span').text,
+                               'away_score':g[1].find_element(By.TAG_NAME,'span').text
+                              }
+                main_dict[str(year)+'_'+str(round)+'_'+str(i+1)] = detail_dict
+            except:
+                pass
+            
+            i+=1
+  
+table_data = pd.DataFrame.from_dict(main_dict).transpose()
+table_data.to_csv('CSV_PATH')
