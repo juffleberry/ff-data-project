d8288277def91c4f532560f421a87fcbac0ef73e
Alistair Taylor <alistairwilliamtaylor@gmail.com>
2022-11-15 21:58:19 +1100

    dropdown box selects team

--- ff-front-end/src/List.js
+++ ff-front-end/src/List.js
@@ -5,21 +5,25 @@ import ListItemText from "@mui/material/ListItemText";
 import ListItemAvatar from "@mui/material/ListItemAvatar";
 import Avatar from "@mui/material/Avatar";
 import SportsFootballIcon from "@mui/icons-material/SportsFootball";
-import records from "./league_records.json";
 
-export default function OpponentList() {
-  return records.TheClairevilleCougars.opponentHistory.map((entry) => {
-    return (
-      <List sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}>
-        <ListItem>
-          <ListItemAvatar>
-            <Avatar>
-              <SportsFootballIcon />
-            </Avatar>
-          </ListItemAvatar>
-          <ListItemText primary={entry.opponent} secondary={entry.record} />
-        </ListItem>
-      </List>
-    );
-  });
+export default function OpponentList({ teamName, records }) {
+  return (
+    teamName &&
+    records[teamName]["opponentHistory"].map((entry) => {
+      return (
+        <List
+          sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}
+        >
+          <ListItem>
+            <ListItemAvatar>
+              <Avatar>
+                <SportsFootballIcon />
+              </Avatar>
+            </ListItemAvatar>
+            <ListItemText primary={entry.opponent} secondary={entry.record} />
+          </ListItem>
+        </List>
+      );
+    })
+  );
 }
